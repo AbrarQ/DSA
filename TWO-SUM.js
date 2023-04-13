825d7509cbd69791ae48b5162520601ffb53eecb
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-04-13 14:19:16 +0530

    Create TWO-SUM.js
    Day-1 Begins

---
+++ TWO-SUM.js
@@ -0,0 +1,26 @@
+//leetcode - 1
+
+/**
+ * Used map to find difference if exists else set nums[i],i
+ * it wont work during the first iteration but it will work from next time.
+ */
+
+
+
+var twoSum = function(nums, target) {
+
+    let map = new Map();
+
+    for(let i=0; i< nums.length;i++){
+        let diff = target-nums[i]
+
+        if( map.has(diff)){
+            return [i, map.get(diff)]
+        }
+
+        map.set(nums[i],i)
+    }
+
+
+  };
+
