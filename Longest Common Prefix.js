42b42ed06a43b054780aa0cc19287acf158c1e45
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-04-20 18:31:29 +0530

    Create Longest Common Prefix.js
    Day-3

---
+++ Longest Common Prefix.js
@@ -0,0 +1,21 @@
+/**
+ * leetcode : 14
+ * author : abrar
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length===0){return ""}
+for(let i=0; i<strs[0].length; i++){
+    const char = strs[0][i]
+    for(let j=1; j<strs.length;j++){
+        if(i===strs[j].length || char != strs[j][i]){
+            return strs[0].substring(0,i)
+        }
+    }
+}
+
+return strs[0]
+
+};
