3db4d5f97e413d3c4f654eb98ed3f621a3f98aa1
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-04-20 19:08:50 +0530

    Day-6

---
+++ Valid Palindrome.js
@@ -0,0 +1,21 @@
+/**
+ *   leetcode :125
+ * author:abrar
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str = s.replace(/[^0-9a-z]/gi,'').toLowerCase()
+    let start =0;
+    let end = str.length-1
+    while(start < end){
+        if(str[start]!= str[end]){
+            return false
+        }
+        start++;
+        end--;
+    }
+ return true
+}
+
+
