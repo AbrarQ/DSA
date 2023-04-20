ec69ac8817828292a6f5fa7c5e09fe4162c0e722
AbrarQ <104406765+AbrarQ@users.noreply.github.com>
2023-04-20 18:33:47 +0530

    Create Valid Parentheses.js
    Day-5

---
+++ Valid Parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * leetcode: 20
+ * author : abrar
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const brackets = {
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+    const stack=[]
+  for (const char of s){
+      if(brackets[char]){
+          stack.push(brackets[char])
+      }else if(stack.pop()!==char){
+          return false;
+      }
+  }
+  return (stack.length===0)
+  };
